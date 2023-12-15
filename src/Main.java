363caec0cf6d71e5a51161ecdbb673ea38b6c0ec
Zuhal <zuhal0202@gmail.com>
2023-12-15 13:38:59 -0600

    Updates

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        /*Scanner scanner = new Scanner(System.in);
         System.out.println("How many favorite things you have?");
         int fv = scanner.nextInt();
         String[] favorite = new String[fv];
@@ -15,6 +15,17 @@ public class Main {
         System.out.println("Your favorite  things are: ");
         for (int i = 0; i < fv; i++) {
             System.out.print(favorite[i] + " ");
-        }
+        }*/
+
+        HumanBeing h1 = new HumanBeing();
+        h1.setWeight(68);
+        h1.setHeight(1.65f);
+        h1.setBmi(calculateBmi(h1));
+        System.out.println("Person has " + h1.getWeight() + " kgs and is " + h1.getHeight() +
+                "meter in height, which results in BMI of " + h1.getBmi());
     }
+        public static float calculateBmi(HumanBeing h1){
+            return h1.getWeight()/ (h1.getHeight()* h1.getHeight());
+        }
+
 }
